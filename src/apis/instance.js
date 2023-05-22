c8dcf6b4c5bc6528e83f30811bfdd055529c043f
ImLxj <1340482172@qq.com>
2023-05-22 17:30:09 +0800

    feat: 分类标题功能的展示

--- src/apis/instance.js
+++ src/apis/instance.js
@@ -2,7 +2,7 @@ import instance from '@/utils/http.js'
 
 /**
  * @description 获取全部分类
- * @return {*} 
+ * @return {*}
  */
 export const getCategoryApi = () => {
   return instance({
@@ -13,12 +13,17 @@ export const getCategoryApi = () => {
 
 /**
  * @description 获取轮播图数据
- * @return {*} 
+ * @return {*}
  */
-export const getBannerApi = () => {
+export const getBannerApi = (params = {}) => {
+  // 默认是1 商品是2
+  const { distributionSite = '1' } = params
   return instance({
     method: 'GET',
-    url: '/home/banner'
+    url: '/home/banner',
+    params: {
+      distributionSite
+    }
   })
 }
 
@@ -52,5 +57,20 @@ export const getGoodsListApi = () => {
   return instance({
     method: 'GET',
     url: '/home/goods'
+  })
+}
+
+/**
+ * @description 获取二级分类列表
+ * @param {*} id
+ * @returns
+ */
+export const getSecondaryCategoryApi = (id) => {
+  return instance({
+    method: 'GET',
+    url: '/category',
+    params: {
+      id
+    }
   })
 }
