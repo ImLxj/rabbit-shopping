145bccfa187aee0d242d56fa9b900812aadb0e03
ImLxj <1340482172@qq.com>
2023-05-22 11:06:59 +0800

    feat: 图片懒加载自定义指令的功能的shi'xi实现

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
@@ -8,9 +7,24 @@ import router from './router'
 // 引入样式文件
 import '@/styles/common.scss'
 
+import { useIntersectionObserver } from '@vueuse/core'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+// 定义全局指令
+app.directive('img-lazy', {
+  mounted(el, binding) {
+    // el: 指令绑定的元素
+    // binding: binding.value 指令等于号后面的绑定的表达式的值
+    useIntersectionObserver(el, ([{ isIntersecting }]) => {
+      if (isIntersecting) {
+        el.src = binding.value
+      }
+    })
+  }
+})
